1dd2a7e403cdd2fb210150227997509378e2cf87
AllenCodess <Aph296@protonmail.com>
2025-09-06 19:22:24 -0400

    added statements to this file

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,23 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature > 80) {
+    console.log("Watering on")
+}
+else {
+    console.log("Water off")
+}
+
+if (timeOfDay === "evening") {
+    console.log("Lights on")
+} else if (timeOfDay === "night") {
+    console.log("Lights on")
+} else {
+    console.log("Lights off")
+}
+
+while (soilMoisture != 45) {
+    console.log(soilMoisture)
+    soilMoisture +=5
+}
